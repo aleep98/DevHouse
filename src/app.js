1f85f918348b3fc67c0f3ba08378ee695620d8c2
aleep98 <zero167@outlook.com>
2025-02-17 20:34:11 -0300

    Conectando com mongoDB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const routes = require('./routes')
+import express from 'express'
+import mongoose from 'mongoose'
+import routes from './routes'
+
 
 class App{
     
     constructor(){
         this.server = express()
+
+        mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.bhkca.mongodb.net/?retryWrites=true&w=majority&appName=DevHouse',{
+        
+        });
 
         this.middlewares()
         this.routes()
@@ -19,4 +25,4 @@ class App{
     }
 }
 
-module.exports = new App().server
+export default new App().server
