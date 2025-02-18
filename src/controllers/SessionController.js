ac171ff842fe7dd36c23997022534667a91f21db
aleep98 <zero167@outlook.com>
2025-02-17 22:05:50 -0300

    Cadastro e login de usuarios

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,31 @@
+// metodos: index, show, update, store, destroy
+
+/*
+index: listagem de sessoes
+store: criar uma sessao
+show: quando queremos listar uma unica sessao
+update: quando queremos alterar alguma sessao
+destroy: quando queremos deletar uma sessao
+*/
+
+import User from "../models/User"
+
+class SessionController{
+
+    async store(req, res){
+
+        const { email } = req.body;
+
+        // verificando se o usuario já existe
+        let user = await User.findOne({ email })
+
+        if(!user){
+
+            user = await User.create({ email })
+        }
+
+        return res.json(user)
+    }
+}
+
+export default new SessionController()
